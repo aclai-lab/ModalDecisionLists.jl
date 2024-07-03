3703aa7d818ca55ee880c1659ac199a5820fd316
giopaglia <24519853+giopaglia@users.noreply.github.com>
2024-07-03 09:31:53 +0200

    Minor clean

--- src/algorithms/sequentialcovering.jl
+++ src/algorithms/sequentialcovering.jl
@@ -16,67 +16,6 @@ using ModalDecisionLists
 using Parameters
 
 ############################################################################################
-################### SequentialCovering - DecisionSet #######################################
-############################################################################################
-
-function sequentialcovering_unordered(
-    X::AbstractLogiset,
-    y::AbstractVector{<:CLabel},
-    w::Union{Nothing,AbstractVector{U},Symbol}=default_weights(length(y));
-    searchmethod::SearchMethod=BeamSearch(),
-    unorderedstrategy::Bool = false,
-    max_rulebase_length::Union{Nothing,Integer}=nothing,
-    min_rule_coverage::Integer=1,
-    suppress_parity_warning::Bool=true,
-    kwargs...
-)::DecisionList where {U<:Real}
-
-    !isnothing(max_rulebase_length) && @assert max_rulebase_length > 0 "`max_rulebase_length` must be  > 0"
-    @assert w isa AbstractVector || w in [nothing, :rebalance, :default]
-
-    w = if isnothing(w) || w == :default
-        default_weights(y)
-    elseif w == :rebalance
-        balanced_weights(y)
-    else
-        w
-    end
-
-    searchmethod = reconstruct(searchmethod,  kwargs)
-
-    !(ninstances(X) == length(y)) && error("Mismatching number of instances between X and y! ($(ninstances(X)) != $(length(y)))")
-    !(ninstances(X) == length(w)) && error("Mismatching number of instances between X and w! ($(ninstances(X)) != $(length(w)))")
-    (ninstances(X) == 0) && error("Empty trainig set")
-
-    y, labels = y |> maptointeger
-
-    nlabels = labels |> length
-
-    uncoveredX = X
-    uncoveredy = y
-    uncoveredw = w
-
-    rulebase = Rule[]
-    for target_class in 1:nlabels
-
-        newrules = find_rules(
-            searchmethod,
-            uncoveredX,
-            uncoveredy,
-            uncoveredw;
-            target_class,
-            nlabels
-        )
-        if !isnothing(max_rulebase_length) && length(rulebase) > (max_rulebase_length - 1)
-            break
-        end
-    end
-
-    defaultconsequent = SoleModels.bestguess(uncoveredy; suppress_parity_warning = suppress_parity_warning)
-    return DecisionList(rulebase, labels[defaultconsequent])
-end
-
-############################################################################################
 ################### SequentialCovering - DecisionList ######################################
 ############################################################################################
 
@@ -105,11 +44,9 @@ This involves iteratively learning a single rule, and removing the newly covered
 
 ```julia-repl
 
-julia> X = PropositionalLogiset(iris_dataframe)
+julia> X = PropositionalLogiset(iris_dataframe);
 
-
-julia> y = Vector{CLabel}(iris_labels)
-
+julia> y = Vector{CLabel}(iris_labels);
 
 julia> sequentialcovering(X, y)
 ▣
@@ -233,13 +170,21 @@ function sequentialcovering(
             justcoveredy = uncoveredy[bestantecedent_coverage]
             justcoveredw = uncoveredw[bestantecedent_coverage]
             consequent_i = SoleModels.bestguess(justcoveredy, justcoveredw; suppress_parity_warning=suppress_parity_warning)
+            prediction = labels[consequent_i]
             info_cm = (;
                 supporting_labels=collect(justcoveredy),
-                supporting_weights=collect(justcoveredw)
+                supporting_weights=collect(justcoveredw),
+                supporting_predictions=Fill(prediction, length(justcoveredy)),
             )
-            consequent_cm = ConstantModel(labels[consequent_i], info_cm)
+            consequent_cm = ConstantModel(prediction, info_cm)
+            # 
+            info_r = (;
+                supporting_labels=collect(uncoveredy),
+                supporting_weights=collect(uncoveredw),
+                supporting_predictions=Fill(prediction, length(uncoveredy)),
+            )
             #
-            (Rule(bestantecedent, consequent_cm), consequent_i)
+            (Rule(bestantecedent, consequent_cm, info_r), consequent_i)
         end
         push!(rulebase, rule)
 
